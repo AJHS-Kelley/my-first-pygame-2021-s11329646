e6b055c737cacc52681e613247b79ad3ca54a389
s11329646 <70536062+s11329646@users.noreply.github.com>
2021-11-30 14:45:04 -0500

    v0.4
    Created boundaries.

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-#My first Pygame, Eriq King, 11/30/2021, v0.3
+#My first Pygame, Eriq King, 11/30/2021, v0.4
 
 import pygame, sys
 from pygame.locals import *
@@ -17,3 +17,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RECLUSE = (132, 24, 93)
+
+# Setup fonts.
+basicFont = pygame.font.SysFont(None, 48)
+
+#setup text.
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
