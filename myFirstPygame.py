5051156fc5dd33b09f462fd40272bb8e97cca1a1
s11329646 <70536062+s11329646@users.noreply.github.com>
2021-11-30 14:26:15 -0500

    v0.2
    Setup game window/ caption

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,12 @@
-#My first Pygame, Eriq King, 11/30/2021, v0.1
+#My first Pygame, Eriq King, 11/30/2021, v0.2
 
 import pygame, sys
 from pygame.locals import *
+
+# Start PyGame
+pygame.init()
+
+#Setup the game window.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32) 
+pygame.display.set_caption('Hello, world!')
+
